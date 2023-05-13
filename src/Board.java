51e3509629d8c5d9fb841da2810823c92c8d4a9d
idanArkav <133385135+idanArkav@users.noreply.github.com>
2023-05-13 19:40:32 +0300

    hello world

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,29 @@
 import java.util.Arrays;
-
 public class Board {
+    private static final int EMPTY=-1;
 Tile[][] tiles;
 
 public Board (String tileString) {
-    //
+    String[] rows = tileString.split("\\|");
+    int rowsNum = rows.length;
+    int colNum = rows[0].split(" ").length;
+    String[][] intialBoard = new String[rowsNum][colNum];
+
+    tiles = new Tile[rowsNum][colNum];
+    for (int i = 0; i < rowsNum; i++) {
+        String row = rows[i];
+        String[] cells = row.split(" ");
+        for (int j = 0; j < colNum; j++) {
+            String cell = cells[j];
+            if (cell.equals("_")) {
+                tiles[i][j] = EMPTY;
+            } else {
+                int value = Integer.parseInt(cell);
+                tiles[i][j] = new Tile(value);
+
+            }
+        }
+    }
 }
     @Override
     public boolean equals(Object other) {
